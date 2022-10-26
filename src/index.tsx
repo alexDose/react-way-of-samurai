256913e8235c2ab92c70cbeea8430430a04dee4f
Alexei <i0ver@mail.ru>
2022-10-26 19:14:34 +0300

    95 lesson HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import './index.css';
 import {store} from "./Redux/reduxStore";
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 export const rerenderEntireTree = () => {
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>,
+        </HashRouter>,
         document.getElementById('root')
     );
 }
